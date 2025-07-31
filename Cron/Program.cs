8ed141225a10c2c44058aa672b5b656462b18cdb
Beauti Sui <183709979+beautisui@users.noreply.github.com>
2025-07-31 15:18:58 +0530

    Adds Program file

--- Cron/Program.cs
+++ Cron/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Cron.Models;
+
+namespace Cron;
+
+class Program
+{
+    private static async Task Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please provide a cron expression");
+            return;
+        }
+        
+        
+        var cron = CronParser.Parse(args[0]);
+        Console.WriteLine($"Parsed Cron -> Minutes: [{string.Join(",", cron.Minutes)}], Hours: [{string.Join(",", cron.Hours)}], DaysOfMonth: [{string.Join(",", cron.DaysOfMonth)}], Months: [{string.Join(",", cron.Months)}], DaysOfWeek: [{string.Join(",", cron.DaysOfWeek)}]");
+        
+        var now = DateTime.Now;
+        
+        var cronSchedular = new CronScheduler();
+        var nextTime = cronSchedular.GetNextExecutionTime(cron, now);
+        
+        Console.WriteLine($"🌼 Next execution call --------> {nextTime} And now is --------------------->  {now}");
+    }
+}
